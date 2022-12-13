bc0204d785a8135eb44cb088bd06219408b2ac2c
Bratkova <bratkova@lancaster.ac.uk>
2022-12-13 16:39:49 +0000

    Add as ggplot

--- UCB_plot.R
+++ UCB_plot.R
@@ -2,4 +2,14 @@ library(tidyverse)
 # UCB algorithm
 source("UCB.R")
 
-UCB(1000,probs=c(0.5,0.55,0.55,0.45))
+# N=1000
+res_1k <- UCB(1000,probs=c(0.5,0.55,0.55,0.45))
+plot(res_1k$chosen)
+hist(res_1k$chosen)
+ggplot(res_1k,aes(x=chosen)) + geom_histogram(binwidth = 0.5,fill="firebrick") 
+
+# N=10000
+res_10k <- UCB(10000,probs=c(0.5,0.55,0.55,0.45))
+plot(res_10k$chosen)
+hist(res_10k$chosen)
+ggplot(res_10k,aes(x=chosen)) + geom_histogram(binwidth = 0.5,fill="firebrick") 
