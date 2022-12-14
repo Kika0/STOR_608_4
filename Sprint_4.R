10da082d76e52dd283f4969285cef9630b9225d9
Bratkova <bratkova@lancaster.ac.uk>
2022-12-14 11:12:00 +0000

    Replace with new version by Robert

--- Sprint_4.R
+++ Sprint_4.R
@@ -1,3 +1,5 @@
+library(invgamma)
+
 # question a
 
 # data for wok iin
@@ -6,13 +8,120 @@ df = data.frame(data = wi)
 write.csv(df, "wokiin.csv")
 plot(density(wi))
 
-
-
 mean = mean(wi)
 var = var(wi)
 
-tau = 2+mean^2/var
-phi = mean*(tau-1)
+mu = mean
+alpha = 100
+beta = 1
+lambda = (alpha-1)/(var*beta)
+
+# normal inverse gamma
+
+rnormgamma <- function(n, mu, lambda, alpha, beta) {
+  if (length(n) > 1) 
+    n <- length(n)
+  sigma2 <- rinvgamma(n, alpha, beta)
+  x <- rnorm(n, mu, sqrt(sigma2/lambda))
+  data.frame(x = x)
+}
+
+plot(density(rnormgamma(10000, mu, lambda, alpha, beta)$x))
+
+### updating for posterior
+
+data = c(9,5,6)
+
+munew = (lambda*mu + sum(data))/(lambda + length(data))
+lambdanew = lambda + length(data)
+alphanew = alpha + length(data)/2
+betanew = beta + 0.5*sum((data - mean(data))^2) + length(data)*lambda/(lambda + length(data))*(mean(data) - mu)^2/2
+
+plot(density(rnormgamma(10000, munew, lambdanew, alphanew, betanew)$x))
+lines(density(rnormgamma(10000, mu, lambda, alpha, beta)$x))
+
+# sampling 1,000,000 datapoints from NIG rv
+
+sample = rnormgamma(10000, munew, lambdanew, alphanew, betanew)$x
+length(sample[sample>8])/length(sample)
+# data for etnas
+
+e <- c()
+for (i in 1:188){
+  e <- c(e, 10)
+}
+for (i in 1:61){
+  e <- c(e, 8)
+}
+for (i in 1:18){
+  e <- c(e, 6)
+}
+for (i in 1:22){
+  e <- c(e, 4)
+}
+for (i in 1:25){
+  e <- c(e, 2)
+}
+
+plot(density(e))
+
+mu = mean(e)
+alpha = 100
+beta = 1
+lambda = (alpha-1)/(var(e)*beta)
+
+plot(density(rnormgamma(10000, mu, lambda, alpha, beta)$x))
+
+### posterior updating
+
+data = c(7,8)
+
+munew = (lambda*mu + sum(data))/(lambda + length(data))
+lambdanew = lambda + length(data)
+alphanew = alpha + length(data)/2
+betanew = beta + 0.5*sum((data - mean(data))^2) + length(data)*lambda/(lambda + length(data))*(mean(data) - mu)^2/2
+
+plot(density(rnormgamma(10000, munew, lambdanew, alphanew, betanew)$x))
+lines(density(rnormgamma(10000, mu, lambda, alpha, beta)$x))
+
+# sampling 1,000,000 datapoints from NIG rv
+
+sample = rnormgamma(10000, munew, lambdanew, alphanew, betanew)$x
+length(sample[sample>8])/length(sample)
+
+# Sultan's reviews
+reviews <- c(rep(10,554),rep(8,138),rep(6,67),rep(4,34),rep(2,52))
+mu <- mean(reviews)
+sig_2 <- var(reviews)
+length(reviews)
+
+mu = mean(reviews)
+alpha = 100
+beta = 1
+lambda = (alpha-1)/(var(reviews)*beta)
+
+plot(density(rnormgamma(10000, mu, lambda, alpha, beta)$x))
+plot(density(reviews))
+
+
+### posterior updating
+
+data = c(3,6,5)
+
+munew = (lambda*mu + sum(data))/(lambda + length(data))
+lambdanew = lambda + length(data)
+alphanew = alpha + length(data)/2
+betanew = beta + 0.5*sum((data - mean(data))^2) + length(data)*lambda/(lambda + length(data))*(mean(data) - mu)^2/2
+
+plot(density(rnormgamma(10000, munew, lambdanew, alphanew, betanew)$x))
+lines(density(rnormgamma(10000, mu, lambda, alpha, beta)$x))
+
+## probability question
+
+# sampling 1,000,000 datapoints from NIG rv
+
+sample = rnormgamma(10000, munew, lambdanew, alphanew, betanew)$x
+length(sample[sample>8])/length(sample)
 
 # question b
 
@@ -91,17 +200,17 @@ res = UCB(df1)
 plot(res$chosen)
 
 UCB(df3)
-res = UCB(df3)
+res = UCB(df1)
 res
 hist(res$chosen)
 
 # calculating regret
 
 ## do this later
-
+res = UCB(df1)
 vec = rep(0, nrow(res))
 for (i in 1:length(vec)){
-  if(res$chosen[i] != 4){
+  if(res$chosen[i] != 2){
     vec[i] = 1
   }
 }
