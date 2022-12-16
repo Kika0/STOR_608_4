95ca6f3d0ff23db7da54bd17439d7e9fa9465ae1
Bratkova <bratkova@lancaster.ac.uk>
2022-12-16 21:36:03 +0000

    Create Thompson sampling script inc df generation

---
+++ TS.R
@@ -0,0 +1,41 @@
+# thompson sampling
+
+TS = function(N,probs, alpha, beta){
+  
+  arms <- length(probs)
+  set.seed(1)
+  # create empty dataframe
+  df <- as.data.frame(matrix(ncol=arms, nrow=N))
+  for (i in 1:arms) {
+    df[,i] <- rbinom(N,1,probs[i]) 
+    #define column names
+    names(df[,i]) <- paste0("bern",i)
+  }  
+  # number of arms
+  k = ncol(df)
+  # number of instances
+  t = nrow(df)
+  # dataframe of hyperparameters
+  hps = data.frame(A = rep(alpha,k), B = rep(beta,k))
+  # recording the chosen arms
+  arms = c()
+  # carrying out TS
+  for(i in 1:t){
+    # creating a list for samples
+    samples = c()
+    # generating beta sample for each arm
+    for(j in 1:k){
+      samples[j] = rbeta(1, hps[j,1], hps[j,2])
+    }
+    # picking the arm with greatest value
+    arm = which.max(samples)
+    # update hyperparameter for that arm
+    # alpha
+    hps[arm,1] = hps[arm,1] + df[i,arm]
+    # beta
+    hps[arm,2] = hps[arm,2] + 1 - df[i,arm]
+    arms = c(arms, arm)
+  }
+  return(data.frame("chosen"=arms))
+  # return(hps)
+}
