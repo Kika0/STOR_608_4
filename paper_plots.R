ad6cc6bd328a3c63dd59ee98192c4553d5209ab7
Bratkova <bratkova@lancaster.ac.uk>
2023-01-14 16:04:11 +0000

    Add plot of KL_UCB

--- paper_plots.R
+++ paper_plots.R
@@ -3,6 +3,7 @@ library(tidyverse)
 source("UCB.R")
 # TS algorithm
 source("TS.R")
+source("KL_UCB.R")
 
 # TS
 ts <- TS(N=1000,probs=c(0.5,0.55),alpha=1,beta=1) %>% mutate("chosen"=as.character(chosen))
@@ -19,9 +20,18 @@ ggplot(ucb,aes(x=chosen)) + geom_histogram(binwidth = 0.5,fill="firebrick",stat=
   ylab("Count") +
   theme_minimal()
 
+# KL-UCB
+# UCB
+kl_ucb <- KL_UCB(1000,probs=c(0.5,0.55)) %>% mutate("chosen"=as.character(chosen))
+ggplot(ucb,aes(x=chosen)) + geom_histogram(binwidth = 0.5,fill="firebrick",stat="count") +
+  xlab("") +
+  ylab("Count") +
+  theme_minimal()
+
 ts <- ts %>% rename("Thompson sampling"=chosen)
 ucb <- ucb %>% rename("UCB"=chosen) 
-all <- cbind(ts,ucb) %>% pivot_longer(c(UCB,"Thompson sampling"),names_to = "algorithm",values_to = "chosen") %>% select(-c(arm,arm.1))
+kl_ucb <- ucb %>% rename("KL-UCB"=chosen)
+all <- cbind(ts,ucb,kl_ucb) %>% pivot_longer(c(UCB,"Thompson sampling"),names_to = "algorithm",values_to = "chosen") %>% select(-c(arm,arm.1))
 
 ggplot(all,aes(x=chosen)) + geom_histogram(binwidth = 0.5,fill="firebrick",stat="count") +
   xlab("") +
