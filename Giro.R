1649062247290e480507506db295ec2676336c5f
Kristina Bratkova <kbratkov0@gmail.com>
2023-01-16 02:39:58 +0000

    Correct Giro algorithm sampling if there is a tie

--- Giro.R
+++ Giro.R
@@ -16,24 +16,35 @@ Giro = function(N,probs){
   # number of instances
   t = nrow(df)
   # dataframe of pseudo rewards
-  hps = data.frame(A = rep(alpha,k), B = rep(beta,k))
+  his = data.frame(matrix(ncol = 2, nrow = 0))
+  # colnames(his) <- c("arm1","arm2")
   # recording the chosen arms
   arms = c()
-  # carrying out TS
+  # carrying out Giro
   for(i in 1:t){
-    # creating a list for samples
-    samples = c()
+    mus = c()
     # generating beta sample for each arm
     for(j in 1:k){
-      samples[j] = rbeta(1, hps[j,1], hps[j,2])
+      if (length(na.omit(his[,j]))> 0) {
+      numerator = sample(x=na.omit(his[,j]),size=length(na.omit(his[,j])),replace = TRUE)
+      denominator = length(na.omit(his[,j]))
+      
+      mu = sum(numerator)/denominator
+      mus = cbind(mus, mu)
+      } else {
+        mu=10^6
+        mus = cbind(mus, mu)
+      }
     }
     # picking the arm with greatest value
-    arm = which.max(samples)
-    # update hyperparameter for that arm
-    # alpha
-    hps[arm,1] = hps[arm,1] + df[i,arm]
-    # beta
-    hps[arm,2] = hps[arm,2] + 1 - df[i,arm]
+    arm = as.numeric(sample(as.character(which(mus == max(mus))),1))
+    # generate pseudo rewards to attach to pseudo history of the chosen arm
+    # note that c(-1,1) are pseudo rewards
+    pseudo_rewards <- c(0,1)
+    rewards <- data.frame(matrix(ncol = 2, nrow = 3))
+    rewards_i= c(df[i,arm],pseudo_rewards)
+    rewards[,arm] <- rewards_i
+    his= rbind(his,rewards)
     arms = c(arms, arm)
   }
   return(cbind(df,data.frame("chosen"=arms)))
